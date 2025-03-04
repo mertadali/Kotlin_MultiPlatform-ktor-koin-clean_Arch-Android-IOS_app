52a5238e4c0a8ab2e1b79541103b112044324db4
mertadali <mertadali605@gmail.com>
2025-03-04 12:53:33 +0300

    first commit

---
+++ shared/build.gradle.kts
@@ -0,0 +1,60 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+
+    //Kotlinx Serialization
+    kotlin("plugin.serialization") version "1.8.0"
+
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_1_8)
+                }
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    //Dependencies versions
+    val coroutinesVersion = "1.6.4"
+    val ktorVersion = "2.2.1"          // -> kotlinden dolayı retrofit yerine kullanılacak.
+    val koinVersion = "3.3.2"          // -> hilt yerine kullanılacak.
+
+
+    sourceSets {
+        commonMain.dependencies {
+            //put your multiplatform dependencies here
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "com.example.moviekmm"
+    compileSdk = 35
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
